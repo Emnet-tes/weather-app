f2611280b82bced165da19cfea3be565d5d7b7ab
Emnet-tes <teshomeemnet255@gmail.com>
2024-05-05 21:23:25 +0300

    removed the unit conversion

--- src/WeatherTemprature.js
+++ src/WeatherTemprature.js
@@ -1,50 +1,13 @@
-import React ,{useState}from "react";
+import React from "react";
 import "./Weather.css";
 
 
-export default function WeatherTemperature(props)
-{
-    let [unit,Setunit] = useState(props.metric);
-    
-    function Changemetrics(event)
-    {
-        event.preventDefault();
-        if (unit === 'celcius'){
-            Setunit("fharniet");
-        }
-        else{
-            Setunit('celcius');
-        }
-    }
-    
-    function tofharniet(){
-        
-        return Math.round((props.celsius * 9 / 5) + 32);
-    }
-    
-        
-        if (unit === "celcius"){
-            return(
-                <div className="tempData">
+export default function WeatherTemperature(props) {
+
+    return (
+        <div className="tempData">
             <span className="tempratue">{Math.round(props.celsius)}</span>
-            <span className="celcious">°C {" | "} 
-            <a href="/" onClick={Changemetrics}>°F</a>
-                    
-            </span></div>
-            )
-            
-        }
-        else{
-            return(
-                <div className="tempData">
-                    <span className="tempratue">{tofharniet()}</span>
-                    <span className="celcious">
-                    <a href="/" onClick={Changemetrics}>°C</a> 
-                    {" | "}°F
-                        
-                    </span></div>
-            );
-                
-        }
-        
+            <span className="celcious">°C </span>
+        </div>
+    );
 }
